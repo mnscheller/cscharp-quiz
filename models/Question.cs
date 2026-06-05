ff4c05980a1d1321c22e6f9ed8862bed856b35d5
Max Scheller <max@netik.de>
2026-06-05 15:23:38 +0200

    REnamed AnswerOptions

--- models/Question.cs
+++ models/Question.cs
@@ -4,14 +4,14 @@ namespace Quizgame.Models
     {
         public int QuestionId { get; init; }
         public string QuestionContent { get; init; }
-        public List<_AntwortOption> AnswerOption { get; init; }
+        public List<AnserOptions> AnswerOption { get; init; }
         public int RightAnswerId { get; init; } //0..3 //option ID
         public string Category { get; init; }
         public Difficulty Difficulty { get; set; }
         public int Point { get; set; }
     }
 
-    public class _AntwortOption
+    public class AnserOptions
     {
         public int OptionID { get; set; }
         public string Option { get; set; }

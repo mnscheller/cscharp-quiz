3b4cde6787ca71a63544a3d033df759fccce33a1
Titusj <bocktitus00@gmail.com>
2026-06-18 14:08:23 +0200

    fix ui

--- ui.cs
+++ ui.cs
@@ -29,24 +29,20 @@ namespace QuizGame.UI
             };
             Console.WriteLine($"Frage: {Question.QuestionContent}");
 
-            for (int i = 0; i < Question.AnswerOption.Count; i++)
+            for (int i = -1; i < Question.AnswerOption.Count; i++)
             {
                 Console.WriteLine($"{i + 1}. {Question.AnswerOption[i]}");
             }
 
             Console.WriteLine("Bitte geben Sie die Nummer der richtigen Antwort ein:");
             string userInput = Console.ReadLine();
-            int UserInputInt = int.Parse(userInput);
-            UserInputInt = UserInputInt--;
-
-            if (UserInputInt == Question.RightAnswerId)
+            if (userInput == Question.RightAnswerId.Parse())
             {
-                Console.WriteLine("Richtig! Sie erhalten 10 Punkte.");
-
+                Console.WriteLine("Die Antwort war richtig! Sie erhalten 10 Punkte.");
             }
             else
             {
-                Console.WriteLine("Falsch! Die richtige Antwort ist: " + "");
+                Console.WriteLine("Die Antwort wahr falsch")
             }
         }
     }
